02748bcf32a774fdf38ddf69871e330370496769
Aline <160494717+lineCrisRS@users.noreply.github.com>
2024-02-26 09:59:39 -0300

    Update exemplo3_processamento.py

--- semana1/exemplo3_processamento.py
+++ semana1/exemplo3_processamento.py
@@ -7,7 +7,7 @@ soma = int(n1) + int(n2)
 
 #int("10") -> Converte o texto "10" para inteiro 10
 #Float("1.5") -> Converte o texto "1.5" para real 1.5
-#str("10") -> Converte o inteuiro "10" para texto 10
+#str("10") -> Converte o inteiro "10" para texto 10
 
 #Saída
 print("A soma é de : ", n1, " + ", n2, " = ", soma)
